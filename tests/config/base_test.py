6e42bd1717553b3cff7ddc710011d64b4e213d69
mycrossstitch <mycrossstitch@users.noreply.github.com>
2025-12-18 13:12:28 +0000

    Deploying to gh-pages from @ mycrossstitch/MyLibrary_test_api@c35537a3e8025bd0ca07330b0ea283c3ee777b71 🚀

--- tests/config/base_test.py
+++
@@ -1,7 +0,0 @@
-from tests.services.books.api_books import BooksAPI
-
-
-class BaseTest:
-
-    def setup_method(self):
-        self.api_books = BooksAPI()
--- tests/config/headers.py
+++
@@ -1,9 +0,0 @@
-import os
-from dotenv import load_dotenv
-
-load_dotenv()
-
-
-class Headers:
-
-    basic = {"Authorization": f"Bearer {os.getenv('API_TOKEN')}"}
